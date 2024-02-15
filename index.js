b8d8d3d27cc7247d43abf9ed99162c0c923beac5
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-02-15 18:19:49 +0200

    feat: create files that should be shown when the user changes the link

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.send('Hello, world!')
+  //console.log(req)
 
-  res.end()
+  // writing the response
+   res.setHeader('Content-Type', 'text/html')
+  fs.readFile('./views/index.html', (error, data) => {
+    if(error) {
+      console.log(`error: ${error}`)
+      res.end()
+    } else {
+      res.write(data)
+      res.end()
+    }
+  })
 })
- server.listen(3000)
+server.listen(3000)
