9835d2fa4dab8cd63d28861c2a147f41149b4f0e
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-02-21 17:34:51 +0200

    feat: create the app from express and also listen to the app on port 8000

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000')
+})
