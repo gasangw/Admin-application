26969fb9ba7673d567f04ad712ca9d49c897e420
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-02-21 17:39:58 +0200

    feat: add a route to the default page

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+  origin: ['http://localhost:3000']
+}))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World')
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000')
