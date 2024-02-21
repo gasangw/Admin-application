192ad6c508a8cb8bd2a4667975be2c3df832bde5
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-02-21 18:23:42 +0200

    refactor: add the validation for the request body

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
+import { RegisterValidation } from "../validation/register.validation";
 
 export const Register = (req:Request, res:Response) => {
-  res.send(req.body)
+  const body = req.body;
+
+  const { error } = RegisterValidation.validate(body);
+
+  if(error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  res.send(body)
 }
